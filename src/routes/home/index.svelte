e2e20da7437760648b1b0726a8514bf4ec0cfac8
Edwin De La Cruz <edwinspire@gmail.com>
2020-11-06 00:34:21 -0500

    Implementación de Service Worker 5

--- src/routes/home/index.svelte
+++ src/routes/home/index.svelte
@@ -4,7 +4,7 @@
   import Watched from "../../components/Watched/Main.svelte";
   import Events from "../../components/Events/Main.svelte";
   import MapAccount from "../../components/Map/Map.svelte";
-  import { subscription, registration } from "../web-push-client.js";
+  import { subscription } from "../web-push-client.js";
 
 
 //  let FData = new FetchData();
