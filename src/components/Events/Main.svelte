926952d036d11234aead50979d156b288211bac7
Edwin De La Cruz <edwinspire@gmail.com>
2020-11-19 23:26:51 -0500

    Se logra colocar puntos en el mapa 7

--- src/components/Events/Main.svelte
+++ src/components/Events/Main.svelte
@@ -84,7 +84,7 @@ box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.75);
   </div>
 </nav>
 <p>
-  <WMap points={details.geo}></WMap>
+  <WMap points={[details.geo.latitude, details.geo.longitude]}></WMap>
   {label} reportado
   {description}
   {JSON.stringify(details)}
