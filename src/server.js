a431e1ced754df6b67787caac3713c11fd579fc7
Edwin De La Cruz <edwinspire@gmail.com>
2021-01-02 20:41:27 -0500

    WebSocket versión Cluster + Arduino

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+//-- No tiene WebSocket funcional por usar Cluster --//
 require("dotenv").config({ override: true });
 const fn_set_expired_lifetime = require("@tasks/fn_set_expired_lifetime");
 const fn_sendemail = require("@tasks/fn_sendemail");
 const { pgWebPush } = require("@app_express_routes/webpush.js");
+const cluster = require("cluster");
 
-//import sio from "socket.io";
+//import * as sio from "socket.io";
 import * as sapper from "@sapper/server";
 import sirv from "sirv";
 import compression from "compression";
@@ -13,9 +15,7 @@ import fs from "fs";
 var ExpiredEventsRunning = false;
 var SendEmailRunning = false;
 global.fecha = new Date();
-
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === "development";
+var ListSockets = [];
 
 // Para generar los certificados correr el siguiente comando, completar los datos que solicita y copiar los dos archivos que se generan
 // openssl req -x509 -nodes -days 1825 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt
@@ -23,75 +23,119 @@ var privateKey = fs.readFileSync("./certs/selfsigned.key", "utf8");
 var certificate = fs.readFileSync("./certs/selfsigned.crt", "utf8");
 var credentials = { key: privateKey, cert: certificate, requestCert: false };
 
-var express = require("express");
+const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
-let app = express(); //instancia de express
-//let httpServer = require("https").createServer(credentials, app);
-let httpServer = require("http").Server(app);
-let io = require("socket.io")(httpServer);
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === "development";
 
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ strict: false, limit: 50000000 })); //-- Limit 50M
-app.use(cookieParser());
-app.use(compression());
-
-app.use(virtual_route);
-app.use(
-  compression({ threshold: 0 }),
-  sirv("static", { dev }),
-  sapper.middleware()
-);
-
-console.log(process.env.LOCAL_SERVER, process.env.DATABASE_URL);
-
-//const PORT = process.env.PORT || 5000 // Esto lo define Heroku
-//process.env.DATABASE_URL =  'postgresql://dbuser:secretpassword@database.server.com:3211/mydb';
-
-io.use((socket, next) => {
-  //socket.handshake.headers.accept = "*/*";
-  //socket.handshake.headers.cookie = 'io=RTXzGzIKwtS7669bAAAA';
-  //socket.handshake.xdomain = false;
-
-  console.log(socket);
-  next();
-});
-
-io.on("error", function (socket) {
-  console.log("Error", socket);
-});
-
-io.on("message", (msj) => {
-  console.log(msj);
-});
-
-io.on("connect_error", (error) => {
-  console.trace(error);
-});
-
-io.on("connection", (socket) => {
-  console.log("Connection", socket.id); // ojIckSD2jqNzOqIrAGzL
+// Esto es para que se ejecute solo en el master y no en los workers
+if (cluster.isMaster) {
+  new pgWebPush();
 
   setInterval(() => {
-    socket.emit("chat", "Hola mundo " + socket.id);
-  }, 2000);
+    if (!SendEmailRunning) {
+      SendEmailRunning = true;
+      fn_sendemail()
+        .then((ret) => {
+          console.log("fn_sendemail End", ret);
+          SendEmailRunning = false;
+        })
+        .catch((e) => {
+          console.log("fn_sendemail Error", e);
+          SendEmailRunning = false;
+        });
+    } else {
+      console.log("fn_sendemail Running...");
+    }
+  }, 1000 * 300);
 
-  socket.send("Al fin");
-  io.send("Al fin 2");
+  setInterval(() => {
+    if (!ExpiredEventsRunning) {
+      ExpiredEventsRunning = true;
+      fn_set_expired_lifetime()
+        .then((ret) => {
+          console.log("ExpiredEvents Running", ret);
+          ExpiredEventsRunning = false;
+        })
+        .catch((e) => {
+          console.log("ExpiredEvents Running Error", e);
+          ExpiredEventsRunning = false;
+        });
+    } else {
+      console.log("ExpiredEvents Running...");
+    }
+  }, 300 * 1000);
+}
 
-  socket.on("chat", (c) => {
-    console.log("Ha recibido chat", socket.id);
-    //socket.emit('test', socket.id);
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  var cpuCount = require("os").cpus().length;
+
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+} else {
+  const app = express(); //instancia de express
+  app.use(morgan("dev"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ strict: false, limit: 50000000 })); //-- Limit 50M
+  app.use(cookieParser());
+  app.use(compression());
+  app.use(virtual_route);
+  app.use(
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  );
+
+  console.log(process.env.LOCAL_SERVER, process.env.DATABASE_URL);
+
+  let httpServer;
+
+  if (!process.env.LOCAL_SERVER) {
+    httpServer = require("http").createServer(app);
+  } else {
+    httpServer = require("https").createServer(credentials, app);
+  }
+
+  let io = require("socket.io")(httpServer);
+
+  /*
+  io.use((socket, next) => {
+    console.log(socket);
+    next();
+  });
+*/
+
+  //io.listen();
+  io.on("error", (e) => {
+    console.trace(e);
   });
 
-  socket.on("disconnect", () => {
-    console.log("Usuario desconectado", socket.id);
-    //socket.emit('test', socket.id);
+  httpServer.on("error", (e) => {
+    console.trace(e);
   });
-});
 
-httpServer.listen(PORT, () => {
-  console.log("App listening on port " + PORT);
+  io.on("connection", (socket) => {
+    socket.emit("chat", "Bienvenido " + socket.id);
+
+    setInterval(() => {
+      socket.emit("chat", new Date() + " - " + socket.id);
+    }, 10 * 1000);
+  });
+
+  httpServer.listen(PORT, () => {
+    console.log("App listening on port " + PORT + " " + cluster.worker.id);
+  });
+}
+
+// Listen for dying workers
+cluster.on("exit", function (worker) {
+  // Replace the dead worker,
+  // we're not sentimental
+  console.log("Worker %d died :(", worker.id);
+  cluster.fork();
 });
