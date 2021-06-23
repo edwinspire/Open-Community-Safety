ee4e937efe2ff516e419d30974991b771e47db5d
Edwin De La Cruz <edwinspire@gmail.com>
2021-06-23 00:10:20 -0500

    En vista que en Heroku el inconveniente se dió por usar Node en Cluster y la cantidad de conexiones a la base de datos se decidió hacer cambios en la aplicación para que use solo un hilo

--- src/server-not-cluster.js
+++ src/server-not-cluster.js
@@ -15,42 +15,19 @@ var certificate = fs.readFileSync("./certs/selfsigned.crt", "utf8");
 var credentials = { key: privateKey, cert: certificate, requestCert: false };
 
 let iToken = new Token();
-let CommunitySafetyBot = new community_safety();
-//const FDataNode = new FetchDataNode();
-
 iToken.deleteAll();
-  CommunitySafetyBot.launch();
 
-  let ServerInstance = new Server({
-    credentials: credentials,
-    listen_notification_list: ["events.data", "test"],
-  });
+let CommunitySafetyBot = new community_safety();
+CommunitySafetyBot.launch();
 
-  let pgNotifyProcess = {};
-  pgNotifyProcess["test"] = (notify) => {
-    console.log("NOTIFY TEST", notify);
-  };
-
-  pgNotifyProcess["events.data"] = async (notify) => {
-    try {
-      let payloadNotify = JSON.parse(notify.payload);
-      if (payloadNotify && payloadNotify.idevent && payloadNotify.idevent > 0) {
-        let event = await community_safety.getEventById(payloadNotify.idevent);
-        community_safety.EmitEventToNameSpace(ServerInstance.socketio, event);
-        //CommunitySafetyBot.sendMessageToGroup(event);
-      } else {
-        console.error('pgNotifyProcess - El evento no es válido', notify);
-      }
-    } catch (error) {
-      console.log('ServerInstance Error: ', error);
-    }
-  };
-
-  ServerInstance.on("pgNotify", (notify) => {
-    pgNotifyProcess[notify.channel](notify);
-  });
+let ServerInstance = new Server({
+  credentials: credentials,
+  listen_notification_list: ["events.data", "test"],
+});
 
 
-  community_safety.CreateSocketIONameSpace(ServerInstance.socketio);
+community_safety.CreateSocketIONameSpace(ServerInstance.socketio);
 
-
+ServerInstance.on("pgNotify", (notify) => {
+  CommunitySafetyBot.ProcessPgNotification(ServerInstance.socketio, notify);
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ override: true });
 const { WITH_NODE_CLUSTER } = process.env;
+console.log('WITH_NODE_CLUSTER: '+WITH_NODE_CLUSTER);
 if (WITH_NODE_CLUSTER === "true") {
   require("./server-cluster");
 } else {
