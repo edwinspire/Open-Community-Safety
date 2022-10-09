e0355c9301c9ab78b49f44591a97ec620672d30d
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-10-09 01:51:17 -0500

    modulos

---
+++ .svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,324 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').ACLOCAL_PATH} */
+export const ACLOCAL_PATH = "C:\\Program Files\\Git\\mingw64\\share\\aclocal;C:\\Program Files\\Git\\usr\\share\\aclocal";
+
+/** @type {import('$env/static/private').ALLUSERSPROFILE} */
+export const ALLUSERSPROFILE = "C:\\ProgramData";
+
+/** @type {import('$env/static/private').APPDATA} */
+export const APPDATA = "C:\\Users\\edelacruz\\AppData\\Roaming";
+
+/** @type {import('$env/static/private').ChocolateyInstall} */
+export const ChocolateyInstall = "C:\\ProgramData\\chocolatey";
+
+/** @type {import('$env/static/private').CHROME_CRASHPAD_PIPE_NAME} */
+export const CHROME_CRASHPAD_PIPE_NAME = "\\\\.\\pipe\\crashpad_32376_WFUDXHGHZJRCNIGH";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').COLORTERM} */
+export const COLORTERM = "truecolor";
+
+/** @type {import('$env/static/private').COMMONPROGRAMFILES} */
+export const COMMONPROGRAMFILES = "C:\\Program Files\\Common Files";
+
+/** @type {import('$env/static/private').CommonProgramW6432} */
+export const CommonProgramW6432 = "C:\\Program Files\\Common Files";
+
+/** @type {import('$env/static/private').COMPUTERNAME} */
+export const COMPUTERNAME = "EDELACRUZ-LP";
+
+/** @type {import('$env/static/private').COMSPEC} */
+export const COMSPEC = "C:\\Windows\\system32\\cmd.exe";
+
+/** @type {import('$env/static/private').CONFIG_SITE} */
+export const CONFIG_SITE = "C:/Program Files/Git/etc/config.site";
+
+/** @type {import('$env/static/private').DISPLAY} */
+export const DISPLAY = "needs-to-be-defined";
+
+/** @type {import('$env/static/private').DriverData} */
+export const DriverData = "C:\\Windows\\System32\\Drivers\\DriverData";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "notepad.exe";
+
+/** @type {import('$env/static/private').EXEPATH} */
+export const EXEPATH = "C:\\Program Files\\Git\\bin";
+
+/** @type {import('$env/static/private').FPS_BROWSER_APP_PROFILE_STRING} */
+export const FPS_BROWSER_APP_PROFILE_STRING = "Internet Explorer";
+
+/** @type {import('$env/static/private').FPS_BROWSER_USER_PROFILE_STRING} */
+export const FPS_BROWSER_USER_PROFILE_STRING = "Default";
+
+/** @type {import('$env/static/private').GIT_ASKPASS} */
+export const GIT_ASKPASS = "c:\\Users\\edelacruz\\AppData\\Local\\Programs\\Microsoft VS Code\\resources\\app\\extensions\\git\\dist\\askpass.sh";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "C:\\Users\\edelacruz";
+
+/** @type {import('$env/static/private').HOMEDRIVE} */
+export const HOMEDRIVE = "C:";
+
+/** @type {import('$env/static/private').HOMEPATH} */
+export const HOMEPATH = "\\Users\\edelacruz";
+
+/** @type {import('$env/static/private').HOSTNAME} */
+export const HOSTNAME = "edelacruz-lp";
+
+/** @type {import('$env/static/private').INFOPATH} */
+export const INFOPATH = "C:\\Program Files\\Git\\usr\\local\\info;C:\\Program Files\\Git\\usr\\share\\info;C:\\Program Files\\Git\\usr\\info;C:\\Program Files\\Git\\share\\info";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "D:\\edwinspire\\proyectos\\Node\\Open-Community-Safety";
+
+/** @type {import('$env/static/private').IWBPath} */
+export const IWBPath = "C:\\Program Files (x86)\\SAP\\FrontEnd\\iwb";
+
+/** @type {import('$env/static/private').LANG} */
+export const LANG = "es_ES.UTF-8";
+
+/** @type {import('$env/static/private').LOCALAPPDATA} */
+export const LOCALAPPDATA = "C:\\Users\\edelacruz\\AppData\\Local";
+
+/** @type {import('$env/static/private').LOGONSERVER} */
+export const LOGONSERVER = "\\\\DCFARMA03";
+
+/** @type {import('$env/static/private').MANPATH} */
+export const MANPATH = "C:\\Program Files\\Git\\mingw64\\local\\man;C:\\Program Files\\Git\\mingw64\\share\\man;C:\\Program Files\\Git\\usr\\local\\man;C:\\Program Files\\Git\\usr\\share\\man;C:\\Program Files\\Git\\usr\\man;C:\\Program Files\\Git\\share\\man";
+
+/** @type {import('$env/static/private').MINGW_CHOST} */
+export const MINGW_CHOST = "x86_64-w64-mingw32";
+
+/** @type {import('$env/static/private').MINGW_PACKAGE_PREFIX} */
+export const MINGW_PACKAGE_PREFIX = "mingw-w64-x86_64";
+
+/** @type {import('$env/static/private').MINGW_PREFIX} */
+export const MINGW_PREFIX = "C:/Program Files/Git/mingw64";
+
+/** @type {import('$env/static/private').MSYSTEM} */
+export const MSYSTEM = "MINGW64";
+
+/** @type {import('$env/static/private').MSYSTEM_CARCH} */
+export const MSYSTEM_CARCH = "x86_64";
+
+/** @type {import('$env/static/private').MSYSTEM_CHOST} */
+export const MSYSTEM_CHOST = "x86_64-w64-mingw32";
+
+/** @type {import('$env/static/private').MSYSTEM_PREFIX} */
+export const MSYSTEM_PREFIX = "C:/Program Files/Git/mingw64";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "C:\\Program Files\\nodejs\\node.exe";
+
+/** @type {import('$env/static/private').NODE_ENV} */
+export const NODE_ENV = "production";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "run-script";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "C:\\Users\\edelacruz\\AppData\\Local\\npm-cache";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "C:\\Users\\edelacruz\\AppData\\Roaming\\npm\\etc\\npmrc";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "C:\\Users\\edelacruz\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "C:\\Users\\edelacruz\\.npm-init.js";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "D:\\edwinspire\\proyectos\\Node\\Open-Community-Safety";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "C:\\Program Files\\nodejs\\node_modules\\npm\\node_modules\\node-gyp\\bin\\node-gyp.js";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "C:\\Users\\edelacruz\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "C:\\Users\\edelacruz\\.npmrc";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/8.11.0 node/v16.16.0 win32 x64 workspaces/false";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "build";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "vite build";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "C:\\Program Files\\nodejs\\node.exe";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "D:\\edwinspire\\proyectos\\Node\\Open-Community-Safety\\package.json";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "my-app";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "0.0.1";
+
+/** @type {import('$env/static/private').NUMBER_OF_PROCESSORS} */
+export const NUMBER_OF_PROCESSORS = "16";
+
+/** @type {import('$env/static/private').OneDrive} */
+export const OneDrive = "C:\\Users\\edelacruz\\OneDrive";
+
+/** @type {import('$env/static/private').ORIGINAL_PATH} */
+export const ORIGINAL_PATH = "C:\\Program Files\\Git\\mingw64\\bin;C:\\Program Files\\Git\\usr\\bin;C:\\Users\\edelacruz\\bin;C:\\Python310\\Scripts;C:\\Python310;C:\\Program Files (x86)\\VMware\\VMware Player\\bin;C:\\Windows\\system32;C:\\Windows;C:\\Windows\\System32\\Wbem;C:\\Windows\\System32\\WindowsPowerShell\\v1.0;C:\\Windows\\System32\\OpenSSH;C:\\Program Files (x86)\\Microsoft SQL Server\\160\\DTS\\Binn;C:\\Program Files\\Azure Data Studio\\bin;C:\\Program Files\\Git\\cmd;C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn;C:\\Program Files (x86)\\Microsoft SQL Server\\150\\Tools\\Binn;C:\\Program Files\\Microsoft SQL Server\\150\\Tools\\Binn;C:\\Program Files\\Microsoft SQL Server\\150\\DTS\\Binn;C:\\ProgramData\\chocolatey\\bin;C:\\Program Files\\dotnet;C:\\Program Files\\PuTTY;C:\\Program Files\\Docker\\Docker\\resources\\bin;C:\\ProgramData\\DockerDesktop\\version-bin;C:\\Program Files\\Microsoft SQL Server\\130\\Tools\\Binn;C:\\Program Files\\nodejs;C:\\Users\\edelacruz\\AppData\\Local\\Microsoft\\WindowsApps;C:\\Users\\edelacruz\\AppData\\Local\\Programs\\Microsoft VS Code\\bin;C:\\Users\\edelacruz\\.dotnet\\tools;C:\\Users\\edelacruz\\AppData\\Roaming\\npm";
+
+/** @type {import('$env/static/private').ORIGINAL_TEMP} */
+export const ORIGINAL_TEMP = "C:/Users/EDELAC~1/AppData/Local/Temp";
+
+/** @type {import('$env/static/private').ORIGINAL_TMP} */
+export const ORIGINAL_TMP = "C:/Users/EDELAC~1/AppData/Local/Temp";
+
+/** @type {import('$env/static/private').ORIGINAL_XDG_CURRENT_DESKTOP} */
+export const ORIGINAL_XDG_CURRENT_DESKTOP = "undefined";
+
+/** @type {import('$env/static/private').OS} */
+export const OS = "Windows_NT";
+
+/** @type {import('$env/static/private').PATH} */
+export const PATH = "D:\\edwinspire\\proyectos\\Node\\Open-Community-Safety\\node_modules\\.bin;D:\\edwinspire\\proyectos\\Node\\node_modules\\.bin;D:\\edwinspire\\proyectos\\node_modules\\.bin;D:\\edwinspire\\node_modules\\.bin;D:\\node_modules\\.bin;C:\\Program Files\\nodejs\\node_modules\\npm\\node_modules\\@npmcli\\run-script\\lib\\node-gyp-bin;C:\\Users\\edelacruz\\bin;C:\\Program Files\\Git\\mingw64\\bin;C:\\Program Files\\Git\\usr\\local\\bin;C:\\Program Files\\Git\\usr\\bin;C:\\Program Files\\Git\\usr\\bin;C:\\Program Files\\Git\\mingw64\\bin;C:\\Program Files\\Git\\usr\\bin;C:\\Users\\edelacruz\\bin;C:\\Python310\\Scripts;C:\\Python310;C:\\Program Files (x86)\\VMware\\VMware Player\\bin;C:\\Windows\\system32;C:\\Windows;C:\\Windows\\System32\\Wbem;C:\\Windows\\System32\\WindowsPowerShell\\v1.0;C:\\Windows\\System32\\OpenSSH;C:\\Program Files (x86)\\Microsoft SQL Server\\160\\DTS\\Binn;C:\\Program Files\\Azure Data Studio\\bin;C:\\Program Files\\Git\\cmd;C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn;C:\\Program Files (x86)\\Microsoft SQL Server\\150\\Tools\\Binn;C:\\Program Files\\Microsoft SQL Server\\150\\Tools\\Binn;C:\\Program Files\\Microsoft SQL Server\\150\\DTS\\Binn;C:\\ProgramData\\chocolatey\\bin;C:\\Program Files\\dotnet;C:\\Program Files\\PuTTY;C:\\Program Files\\Docker\\Docker\\resources\\bin;C:\\ProgramData\\DockerDesktop\\version-bin;C:\\Program Files\\Microsoft SQL Server\\130\\Tools\\Binn;C:\\Program Files\\nodejs;C:\\Users\\edelacruz\\AppData\\Local\\Microsoft\\WindowsApps;C:\\Users\\edelacruz\\AppData\\Local\\Programs\\Microsoft VS Code\\bin;C:\\Users\\edelacruz\\.dotnet\\tools;C:\\Users\\edelacruz\\AppData\\Roaming\\npm;C:\\Program Files\\Git\\usr\\bin\\vendor_perl;C:\\Program Files\\Git\\usr\\bin\\core_perl";
+
+/** @type {import('$env/static/private').PATHEXT} */
+export const PATHEXT = ".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.PY;.PYW";
+
+/** @type {import('$env/static/private').PKG_CONFIG_PATH} */
+export const PKG_CONFIG_PATH = "C:\\Program Files\\Git\\mingw64\\lib\\pkgconfig;C:\\Program Files\\Git\\mingw64\\share\\pkgconfig";
+
+/** @type {import('$env/static/private').PLINK_PROTOCOL} */
+export const PLINK_PROTOCOL = "ssh";
+
+/** @type {import('$env/static/private').PROCESSOR_ARCHITECTURE} */
+export const PROCESSOR_ARCHITECTURE = "AMD64";
+
+/** @type {import('$env/static/private').PROCESSOR_IDENTIFIER} */
+export const PROCESSOR_IDENTIFIER = "AMD64 Family 23 Model 96 Stepping 1, AuthenticAMD";
+
+/** @type {import('$env/static/private').PROCESSOR_LEVEL} */
+export const PROCESSOR_LEVEL = "23";
+
+/** @type {import('$env/static/private').PROCESSOR_REVISION} */
+export const PROCESSOR_REVISION = "6001";
+
+/** @type {import('$env/static/private').ProgramData} */
+export const ProgramData = "C:\\ProgramData";
+
+/** @type {import('$env/static/private').PROGRAMFILES} */
+export const PROGRAMFILES = "C:\\Program Files";
+
+/** @type {import('$env/static/private').ProgramW6432} */
+export const ProgramW6432 = "C:\\Program Files";
+
+/** @type {import('$env/static/private').PROMPT} */
+export const PROMPT = "$P$G";
+
+/** @type {import('$env/static/private').PSModulePath} */
+export const PSModulePath = "%ProgramFiles%\\WindowsPowerShell\\Modules;C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\Modules;C:\\Program Files (x86)\\Microsoft SQL Server\\150\\Tools\\PowerShell\\Modules\\";
+
+/** @type {import('$env/static/private').PUBLIC} */
+export const PUBLIC = "C:\\Users\\Public";
+
+/** @type {import('$env/static/private').PWD} */
+export const PWD = "D:/edwinspire/proyectos/Node/Open-Community-Safety";
+
+/** @type {import('$env/static/private').SESSIONNAME} */
+export const SESSIONNAME = "Console";
+
+/** @type {import('$env/static/private').SHELL} */
+export const SHELL = "C:\\Program Files\\Git\\usr\\bin\\bash.exe";
+
+/** @type {import('$env/static/private').SHLVL} */
+export const SHLVL = "2";
+
+/** @type {import('$env/static/private').SNC_LIB} */
+export const SNC_LIB = "C:\\Program Files (x86)\\SAP\\SNCEncryption\\x86\\sapsncencryption.dll";
+
+/** @type {import('$env/static/private').SNC_LIB_64} */
+export const SNC_LIB_64 = "C:\\Program Files (x86)\\SAP\\SNCEncryption\\x64\\sapsncencryption.dll";
+
+/** @type {import('$env/static/private').SSH_ASKPASS} */
+export const SSH_ASKPASS = "C:/Program Files/Git/mingw64/bin/git-askpass.exe";
+
+/** @type {import('$env/static/private').SYSTEMDRIVE} */
+export const SYSTEMDRIVE = "C:";
+
+/** @type {import('$env/static/private').SYSTEMROOT} */
+export const SYSTEMROOT = "C:\\Windows";
+
+/** @type {import('$env/static/private').TEMP} */
+export const TEMP = "C:\\Users\\EDELAC~1\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').TERM} */
+export const TERM = "xterm-256color";
+
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "vscode";
+
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "1.69.2";
+
+/** @type {import('$env/static/private').TMP} */
+export const TMP = "C:\\Users\\EDELAC~1\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').TMPDIR} */
+export const TMPDIR = "C:\\Users\\EDELAC~1\\AppData\\Local\\Temp";
+
+/** @type {import('$env/static/private').USERDNSDOMAIN} */
+export const USERDNSDOMAIN = "FARMAENLACE.COM";
+
+/** @type {import('$env/static/private').USERDOMAIN} */
+export const USERDOMAIN = "FARMAENLACE0";
+
+/** @type {import('$env/static/private').USERDOMAIN_ROAMINGPROFILE} */
+export const USERDOMAIN_ROAMINGPROFILE = "FARMAENLACE0";
+
+/** @type {import('$env/static/private').USERNAME} */
+export const USERNAME = "edelacruz";
+
+/** @type {import('$env/static/private').USERPROFILE} */
+export const USERPROFILE = "C:\\Users\\edelacruz";
+
+/** @type {import('$env/static/private').VBOX_MSI_INSTALL_PATH} */
+export const VBOX_MSI_INSTALL_PATH = "C:\\Program Files\\Oracle\\VirtualBox\\";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_EXTRA_ARGS} */
+export const VSCODE_GIT_ASKPASS_EXTRA_ARGS = "--ms-enable-electron-run-as-node";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_MAIN} */
+export const VSCODE_GIT_ASKPASS_MAIN = "c:\\Users\\edelacruz\\AppData\\Local\\Programs\\Microsoft VS Code\\resources\\app\\extensions\\git\\dist\\askpass-main.js";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_NODE} */
+export const VSCODE_GIT_ASKPASS_NODE = "C:\\Users\\edelacruz\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe";
+
+/** @type {import('$env/static/private').VSCODE_GIT_IPC_HANDLE} */
+export const VSCODE_GIT_IPC_HANDLE = "\\\\.\\pipe\\vscode-git-94ab2bbce2-sock";
+
+/** @type {import('$env/static/private').WINDIR} */
+export const WINDIR = "C:\\Windows";
+
+/** @type {import('$env/static/private')._} */
+export const _ = "C:/Program Files/nodejs/node.exe";
+
+/** @type {import('$env/static/private').BOT_TOKEN} */
+export const BOT_TOKEN = "1878582988:AAH3Q1j5LzAo8cEBXYtMEEEy4swosLq6SZ8";
+
+/** @type {import('$env/static/private').PORT} */
+export const PORT = "3000";
