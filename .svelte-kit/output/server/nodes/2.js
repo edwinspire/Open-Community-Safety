bf6e6de729fc6f68a313001af96daac332ff43bd
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-12-03 17:31:56 -0500

    Funcional 2

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/about.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = '_app/immutable/pages/about.svelte-6f0a7c88.js';
-export const imports = ["_app/immutable/pages/about.svelte-6f0a7c88.js","_app/immutable/chunks/index-b1fe9a41.js"];
-export const stylesheets = ["_app/immutable/assets/about-9682aba9.css"];
